da4c60fb93e52153872e144571e7cf5075b10a34
Ian He <39037239+ianhe8x@users.noreply.github.com>
2022-09-02 09:15:49 +1200

    improve example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -16,16 +16,23 @@ provider "onfinality" {
 }
 
 resource "onfinality_node" "n1" {
+  # Workspace id, can get it from url https://app.onfinality.io/workspaces/<workspace_id>/nodes
   workspace_id     = 6635707676612587520
+  # Network of the node, can get from `onf network-spec list` & `onf network-spec list-backups`
   network_spec_key = "polkadot"
+  # Node Spec of the node, always put key="unit", 1 * unit ~ 0.5 cpu 1.5G mem
   node_spec = {
     key        = "unit"
     multiplier = 4
   }
+  # full or archive or validator, depends on network
   node_type     = "full"
   node_name     = "ian test2"
+  # Cluster where the node will be deployed, check `onf info cluster` for all available clusters
   cluster_hash  = "jm"
+  # Disk size of the node, <num>Gi , e.g 100Gi
   storage       = "100Gi"
   image_version = "v0.9.27"
-  stopped       = false
+  # <Optional> Change it to true will stop the node
+  # stopped = false
 }
